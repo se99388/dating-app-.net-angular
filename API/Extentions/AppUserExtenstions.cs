3086ca22b34c11c09933ea018a0ff8ed7c7fbc8b
ofir <houbara0@gmail.com>
2025-11-25 17:07:41 +0200

    user interface

--- API/Extentions/AppUserExtenstions.cs
+++ API/Extentions/AppUserExtenstions.cs
@@ -14,6 +14,7 @@ public static class AppUserExtenstions
             Id = user.Id,
             DisplayName = user.DisplayName,
             Email = user.Email,
+            ImageUrl = user.ImageUrl,
             Token = tokenService.CreateToken(user)
         };
     }
---
+++ client/src/core/services/member-service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member, Photo } from '../../types/member';
+import { AccountService } from './account-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  private http = inject(HttpClient);
+  private accountService = inject(AccountService)
+  private baseUrl = environment.apiUrl;
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'members');
+  }
+
+  getMember(id: string) {
+    return this.http.get<Member>(`${this.baseUrl}members/${id}`);
+  }
+
+  getMemberPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
+  }
+}

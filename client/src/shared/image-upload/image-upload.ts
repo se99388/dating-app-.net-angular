87cfd7a07389b2e5297cbc6193f18ab04b0cd831
ofir <houbara0@gmail.com>
2025-11-27 20:18:23 +0200

    onFileSelected method

--- client/src/shared/image-upload/image-upload.ts
+++ client/src/shared/image-upload/image-upload.ts
@@ -56,4 +56,13 @@ export class ImageUpload {
       this.uploadFile.emit(this.fileToUplaod);
     }
   }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.previewImage(file);
+      this.fileToUplaod = file;
+    }
+  }
 }
